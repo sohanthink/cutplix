0114283ad513632c162df640209ad598876b008c
Muhammad Sohan Mollah <sohansourab@gmail.com>
2023-10-25 06:16:45 +0600

    satisfaction part complete

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,7 @@
+// =============================
+// services toggle button js
+// ==========================
 
-// toggle button js
 function showDiv(divNumber) {
   for (let i = 1; i <= 4; i++) {
     const div = document.getElementById(`div${i}`);
@@ -33,3 +35,68 @@ function NestedshowDiv(divNumber) {
     }
   }
 }
+
+
+// =============================
+// satisfaction slider js
+// ==========================
+
+const sliderContainer = document.getElementById('slider-container');
+        const sliderContent = document.getElementById('slider-content');
+        const sliderItems = document.querySelectorAll('.slider-item');
+        const prevBtn = document.getElementById('prevBtn');
+        const nextBtn = document.getElementById('nextBtn');
+        const dots = document.querySelectorAll('.dot');
+
+        let currentIndex = 0;
+        let interval;
+
+        function showCurrentSlide() {
+            sliderContent.style.transform = `translateX(-${currentIndex * 1100}px)`; // Adjust the width (e.g., 300) as needed
+            updateDots();
+        }
+
+        function showNext() {
+            if (currentIndex < sliderItems.length - 1) {
+                currentIndex++;
+                showCurrentSlide();
+            }
+        }
+
+        function showPrevious() {
+            if (currentIndex > 0) {
+                currentIndex--;
+                showCurrentSlide();
+            }
+        }
+
+        function goToSlide(index) {
+            currentIndex = index;
+            showCurrentSlide();
+        }
+
+        function updateDots() {
+            dots.forEach((dot, index) => {
+                dot.classList.toggle('active', index === currentIndex);
+            });
+        }
+
+        prevBtn.addEventListener('click', showPrevious);
+        nextBtn.addEventListener('click', showNext);
+
+        function startAutoplay() {
+            interval = setInterval(showNext, 3000); // Change the interval (in milliseconds) as needed
+        }
+
+        function stopAutoplay() {
+            clearInterval(interval);
+        }
+
+        // Initially display the first slide and start autoplay
+        showCurrentSlide();
+        startAutoplay();
+
+        // Pause autoplay when hovering over the slider
+        sliderContainer.addEventListener('mouseenter', stopAutoplay);
+        sliderContainer.addEventListener('mouseleave', startAutoplay);
+
